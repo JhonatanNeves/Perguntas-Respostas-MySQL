130ba1a172010fd9eb5475f4a798d5cdea0b07e5
jhonatanneves <nevesjn2@gmail.com>
2022-02-16 21:25:48 -0300

    att

--- index.js
+++ index.js
@@ -3,25 +3,10 @@ const app = express();
 
 // ESTOU DIZENDO PARA O EXPRESS USAR O EJS COMO VIEW ENGINE
 app.set('view engine','ejs');
+app.use(express.static('public'));
 
-app.get("/:nome/:lang",(req, res)=>{
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = true;
-
-    var produtos = [
-        {nome:"Doritos", preco:8.99},
-        {nome:"Coca-Lata", preco:5},
-        {nome:"Leite", preco:3.49}
-    ]
-
-    res.render("index",{
-        nome: nome,
-        lang: lang,
-        empresa: "Guia do Jiraya",
-        inscritos: 8000,
-        msg: exibirMsg
-    });
+app.get("/",(req, res)=>{
+    res.render("index");
 });
 
 app.listen(3000,()=>{console.log("App rodando!");});
